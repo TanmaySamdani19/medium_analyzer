63e8aa352db28a03a14f9ef869dddd6c06f132a6
TanmaySamdani19 <tanmaysamdani12345@gmail.com>
2025-06-15 23:52:23 +0100

    loading text

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.document_loaders import Textloader
+from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
@@ -9,3 +9,5 @@ load_dotenv()
 
 if __name__ == '__main__':
     print('Ingesting...')
+    loader = TextLoader("./mediumblog1.txt")
+    document = loader.load()
